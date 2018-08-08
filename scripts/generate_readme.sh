af84b172b09f2c142b30817d6b5457e284af44d9
Marcus Lugg <marcuslugg@googlemail.com>
2018-08-08 17:30:46 +0100

    CLN: Make docs concatenated into README "private"
    Due to the top-level README being constructed simply by concatenating
    several of the files in the  docs directory, the relative path links
    only work in the README. While I look for a better way to solve this,
    I've simply prepended the names of the files the README is generated
    from with an underscore to indicate that they're not supposed to be
    viewed in isolation.

--- scripts/generate_readme.sh
+++ scripts/generate_readme.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-export overview="../docs/overview.md"
-export topics_overview="../docs/topics/topics_overview.md"
-export proposed_projects="../docs/proposed_projects.md"
-export books="../docs/books.md"
-export courses="../docs/courses.md"
-export certification="../docs/certifications.md"
+export overview="../docs/_overview.md"
+export topics_overview="../docs/topics/_topics_overview.md"
+export proposed_projects="../docs/_proposed_projects.md"
+export books="../docs/_books.md"
+export courses="../docs/_courses.md"
+export certification="../docs/_certifications.md"
 
 cat $overview $topics_overview $books $proposed_projects $courses $certification > ../README.md
