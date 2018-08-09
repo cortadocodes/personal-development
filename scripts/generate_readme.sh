eeba9a6e42787d24c1685c28df3b58a20b571bee
Marcus Lugg <marcuslugg@googlemail.com>
2018-08-09 15:45:19 +0100

    FIX: Specify files relative to git root for README
    The location of the  markdown files whos content is used to generate
    the README are now specified relative to the git repository root,
    rather than relative to the scripts directory. This means the
    generate_readme.sh script can now be run from anywhere, not just
    from inside the scripts directory.
    
    Resolves #1

--- scripts/generate_readme.sh
+++ scripts/generate_readme.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-export overview="../docs/_overview.md"
-export topics_overview="../docs/topics/_topics_overview.md"
-export proposed_projects="../docs/_proposed_projects.md"
-export books="../docs/_books.md"
-export courses="../docs/_courses.md"
-export certification="../docs/_certifications.md"
+repository_root="$(git rev-parse --show-toplevel )"
+docs="$repository_root/docs"
+
+export overview="$docs/_overview.md"
+export topics_overview="$docs/topics/_topics_overview.md"
+export proposed_projects="$docs/_proposed_projects.md"
+export books="$docs/_books.md"
+export courses="$docs/_courses.md"
+export certification="$docs/_certifications.md"
 
 cat $overview $topics_overview $books $proposed_projects $courses $certification > ../README.md
