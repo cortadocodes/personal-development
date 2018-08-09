4b1a9e599174985897d80ba102315d2632dde8ce
Marcus Lugg <marcuslugg@googlemail.com>
2018-08-09 15:58:38 +0100

    REF: Remove unnecessary use of export

--- scripts/generate_readme.sh
+++ scripts/generate_readme.sh
@@ -3,11 +3,11 @@
 repository_root="$(git rev-parse --show-toplevel )"
 docs="$repository_root/docs"
 
-export overview="$docs/_overview.md"
-export topics_overview="$docs/topics/_topics_overview.md"
-export proposed_projects="$docs/_proposed_projects.md"
-export books="$docs/_books.md"
-export courses="$docs/_courses.md"
-export certification="$docs/_certifications.md"
+overview="$docs/_overview.md"
+topics_overview="$docs/topics/_topics_overview.md"
+proposed_projects="$docs/_proposed_projects.md"
+books="$docs/_books.md"
+courses="$docs/_courses.md"
+certification="$docs/_certifications.md"
 
 cat $overview $topics_overview $books $proposed_projects $courses $certification > ../README.md
